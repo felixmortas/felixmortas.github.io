3c704605a61259b75b6e9d5de1921f6c5800b1b9
felixmortas <felix.mortas@hotmail.fr>
2026-04-29 01:13:44 -0500

    migrate to l10n website technology

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 // ===== SCRIPT.JS =====
 
-// Form submission handler (front-end only)
+// CHANGE LANGUAGE FUNCTIONNALITY
+
+async function changeLanguage(lang) {
+  // 1. Load language file
+  
+  const response = await fetch(`assets/lang/${lang}.json`);
+  console.log(response);
+  const translations = await response.json();
+
+  // 2. Go throught all the elements having the `data-key` attribute
+  document.querySelectorAll('[data-key]').forEach(element => {
+    const key = element.getAttribute('data-key');
+    if (translations[key]) {
+      element.innerHTML = translations[key];
+    }
+  });
+
+  // Optional : Save the language in the browser
+  localStorage.setItem('preferredLang', lang);
+
+  // BUTTONS MANAGEMENT
+    // First display all the buttons
+    document.querySelectorAll('.other-language-button button').forEach(btn => {
+        btn.style.display = 'inline-block'; 
+    });
+
+    // Hide the button corresponding to the actual language
+    const currentBtn = document.getElementById(`btn-${lang}`);
+    if (currentBtn) {
+        currentBtn.style.display = 'none';
+    }
+}
+
+// When the page is loading, load the saved or default language
+window.onload = () => {
+  const savedLang = localStorage.getItem('preferredLang') || 'en';
+  changeLanguage(savedLang);
+};
+
+
+// FORM SUBMISSION HANDLER (FRONT-END ONLY)
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form');
 
